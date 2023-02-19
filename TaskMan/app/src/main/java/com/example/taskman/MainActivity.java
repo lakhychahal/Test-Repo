b5645478ee6aed6f68bbeaff914edc1fb1d72586
GopikaAjithh <g_ajith@fanshaweonline.ca>
2023-02-19 17:53:58 -0500

    #3
    Editing can now be done

--- TaskMan/app/src/main/java/com/example/taskman/MainActivity.java
+++ TaskMan/app/src/main/java/com/example/taskman/MainActivity.java
@@ -5,6 +5,7 @@ import static com.example.taskman.R.id.task_list_layout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +14,8 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
     private EditText taskEditText;
@@ -38,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
                 if (!task.isEmpty()) {
                     taskList.add(task);
                     addTaskView(task);
+                    taskEditText.setText("");
+                    saveTasks();
 
                 }
             }
@@ -48,8 +53,42 @@ public class MainActivity extends AppCompatActivity {
         TextView taskTextView = taskView.findViewById(R.id.task_text_view);
         taskTextView.setText(task);
 
-
+        Button editButton = taskView.findViewById(R.id.edit_button);
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editTask(taskView, task);
+            }
+        });
 
         taskListLayout.addView(taskView);
     }
+    private void editTask(final View taskView, final String task) {
+        final EditText taskEditText = taskView.findViewById(R.id.task_text_view);
+        final Button editButton = taskView.findViewById(R.id.edit_button);
+
+
+        taskEditText.setEnabled(true);
+        editButton.setText("Save");
+
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newTask = taskEditText.getText().toString();
+                if (!newTask.isEmpty()) {
+                    taskList.set(taskList.indexOf(task), newTask);
+                    taskEditText.setEnabled(false);
+                    editButton.setText("Edit");
+                    saveTasks();
+                }
+            }
+        });
     }
+    private void saveTasks() {
+        SharedPreferences sharedPreferences = getSharedPreferences("tasks", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Set<String> taskSet = new HashSet<>(taskList);
+        editor.putStringSet("taskList", taskSet);
+        editor.apply();
+    }
+}
